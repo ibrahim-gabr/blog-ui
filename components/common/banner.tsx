82b2a6f63a147c72c7ff6071746867ad1bc9e3d0
ibrahim gabr <iageoscience@live.com>
2022-06-03 16:52:09 +0200

    fix baner overlay

--- components/common/banner.tsx
+++ components/common/banner.tsx
@@ -1,17 +1,19 @@
 import React, { FC } from "react";
 import Overlay from "./overlay";
+import OverlayFromRight from "./OverlayFromRight";
 
 type BanerProps = {
   children?: React.ReactNode;
   img: string;
+  overlay?: string;
 };
-const Banner: FC<BanerProps> = ({ children, img }) => {
+const Banner: FC<BanerProps> = ({ children, img, overlay }) => {
   return (
     <div className="relative  ">
       <div className="absolute inset-0">
         <img className="w-full h-full object-cover" src={img} alt="" />
 
-        <Overlay />
+        {overlay == "right" ? <OverlayFromRight /> : <Overlay />}
       </div>
 
       <div className="relative max-w-7xl mx-auto h-[260px] sm:h-[336px] px-4 sm:px-6 lg:px-8 flex flex-col items-start space-y-4  justify-center py-4">
--- components/home/banner.tsx
+++ components/home/banner.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../common/banner";
 import { FC } from "react";
 import { Post } from "~/pages";
+import { useRouter } from "next/router";
 
 export type HomeBannerProps = {
   post: Banner;
@@ -13,19 +14,24 @@ type Banner = {
 };
 
 const HomeBanner: FC<HomeBannerProps> = ({ post }) => {
+  const router = useRouter();
   return (
-    <Banner img={post.image}>
-      <div className="relative justify-start max-w-7xl  h-[264px] sm:h-[500px] px-4 sm:px-6 lg:px-8 flex items-end py-8">
-        <div className="flex flex-col space-y-3">
-          <span className="text-sm text-[#FAFAFA] border-r-2 border-theme-primary pr-2">
-            {post.content.subcategory}
-          </span>
-          <span className="text-white text-xl">{post.content.title}</span>
-          <p className="text-[#ECEEED] text-sm">{post.content.excerpt}</p>
-          <p className="text-[#ECEEED] text-sm">بواسطه {post.content.author}</p>
+    <div onClick={() => router.push(`/studies/${post.content.slug}`)}>
+      <Banner img={post.image} overlay="right">
+        <div className="relative justify-start max-w-7xl  h-[264px] sm:h-[500px] px-4 sm:px-6 lg:px-8 flex items-end py-8">
+          <div className="flex flex-col space-y-3">
+            <span className="text-sm text-[#FAFAFA] border-r-2 border-theme-primary pr-2">
+              {post.content.subcategory}
+            </span>
+            <span className="text-white text-xl">{post.content.title}</span>
+            <p className="text-[#ECEEED] text-sm">{post.content.excerpt}</p>
+            <p className="text-[#ECEEED] text-sm">
+              بواسطه {post.content.author}
+            </p>
+          </div>
         </div>
-      </div>
-    </Banner>
+      </Banner>
+    </div>
   );
 };
 
